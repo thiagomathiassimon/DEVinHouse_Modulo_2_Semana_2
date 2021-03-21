295e30eddb3bebcf33174bdb0ba16ee990a0ed24
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-03-21 20:23:47 -0300

    Exercício 8

---
+++ Exercício_8/Exercicio_8_21_03_2021/src/br/com/devinhouse/exercicioquinze/model/Gerente.java
@@ -0,0 +1,31 @@
+package br.com.devinhouse.exercicioquinze.model;
+
+public class Gerente extends Funcionario {
+
+	private String senha;
+	private int numeroDeFuncionariosGerenciados;
+
+	public Gerente(String nome, String sobrenome, String cpf, double salario, String senha,
+			int numeroDeFuncionariosGerenciados) {
+		super(nome, sobrenome, cpf, salario);
+		this.senha = senha;
+		this.numeroDeFuncionariosGerenciados = numeroDeFuncionariosGerenciados;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public int getNumeroDeFuncionariosGerenciados() {
+		return numeroDeFuncionariosGerenciados;
+	}
+
+	public void setNumeroDeFuncionariosGerenciados(int numeroDeFuncionariosGerenciados) {
+		this.numeroDeFuncionariosGerenciados = numeroDeFuncionariosGerenciados;
+	}
+
+}
